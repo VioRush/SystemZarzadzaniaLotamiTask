edea373ebf71c9d66a292078a0396b02c27d9785
VioRush <violetta.rushnitskaya@gmail.com>
2024-04-20 23:29:22 +0200

    DBConfiguration

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddMvc();
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseInMemoryDatabase("DB"));
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseContext")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
