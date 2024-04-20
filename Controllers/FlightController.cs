ddb86a6bcb13e13f5736e65720c9b9a2defc138a
VioRush <violetta.rushnitskaya@gmail.com>
2024-04-20 17:59:03 +0200

    FlightController

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SystemZarzadzaniaLotami.Models;
+
+namespace SystemZarzadzaniaLotami.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightController : ControllerBase
+    {
+        private readonly DatabaseContext db;
+
+        public FlightController(DatabaseContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Flight>>> GetFlights()
+        {
+            return await db.Flights.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Flight>> GetFlight(int id)
+        {
+            var flight = await db.Flights.FindAsync(id);
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return flight;
+        }
+
+        // PUT: api/Flight/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFlight(int id, Flight flight)
+        {
+            if (id != flight.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(flight).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FlightExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Flight>> AddFlight(Flight flight)
+        {
+            db.Flights.Add(flight);
+            await db.SaveChangesAsync();
+
+            return CreatedAtAction("GetFlight", new { id = flight.Id }, flight);
+        }
+
+        // DELETE: api/Flight/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFlight(int id)
+        {
+            var flight = await db.Flights.FindAsync(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            db.Flights.Remove(flight);
+            await db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FlightExists(int id)
+        {
+            return db.Flights.Any(e => e.Id == id);
+        }
+    }
+}
--- Models/Flight.cs
+++ Models/Flight.cs
@@ -1,4 +1,4 @@
-namespace SystemZarzadzaniaLotami
+namespace SystemZarzadzaniaLotami.Models
 {
     public class Flight
     {
